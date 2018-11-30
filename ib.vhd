a98a3ac8adf66527d202dc77b2e43e48422bc899
MichaelBarrett219 <michaelbarrett000@gmail.com>
2018-11-29 20:04:24 -0500

    fix ib

--- ib.vhd
+++ ib.vhd
@@ -1,35 +1,46 @@
 -- IB
--- note: the PC is not implemented here, it will be input to this module by
--- means of the testbench.
+-- note: the PC is not implemented here, it will be input to this module	 
 
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package ib_type_pkg is
+type ib_type is array(0 to 31) of std_logic_vector (24 downto 0);
+end package ib_type_pkg;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.ib_type_pkg.all;
 
 entity ib is
   port(
     -- inputs
     load_mode : in std_logic; -- 0 = loading data array, 1 = reading from instr_out
     program_counter : in std_logic_vector(4 downto 0); -- 32 instrs, so 5 bits addr
-    data_in : in array (0 to 31) of std_logic_vector(24 downto 0); -- data input
+    data_in : in ib_type; -- data input
     -- outputs
-    instr_out : out std_logic_vector(24 downto 0); -- data out -- 25 bits long
+    instr_out : out std_logic_vector(24 downto 0) -- data out -- 25 bits long
     );
 end entity;
 
 architecture rtl of ib is
 
-  -- array holds 32 25-bit instructions
-  type data_type is array (0 to 31) of std_logic_vector(24 downto 0);
-  signal data : data_type; -- stored data
-  data <= (0 => B"1010_1010_1010_1010_1010_1010", 1 => B"1010", 2 => B"0101", 3 => B"1100", 4 => B"0011", 5 => B"1110", 6 => B"0010");
-  signal read_address : std_logic_vector(4 downto 0); -- 32 instrs
+signal data : ib_type; -- stored data
+signal read_address : std_logic_vector(4 downto 0); -- 32 instrs
 
   begin
 
-    IbProc : process(clock) is
+    IbProc : process(load_mode, program_counter, data_in) is	
 
     begin
+	  -- array holds 32 25-bit instructions
+      --type data_type is array (0 to 31) of std_logic_vector(24 downto 0);
+      
+      data <= (0 => B"1010_1010_1010_1010_1010_10101", 1 => B"1010010101010101010101010", others => B"0000000000000000000000000");
+	
+		
       if load_mode = '0' then
         data <= data_in;
       end if;
@@ -39,6 +50,4 @@ architecture rtl of ib is
 
     end process IBProc;
 
-  end architecture rtl;
-
-    
+end architecture rtl;
