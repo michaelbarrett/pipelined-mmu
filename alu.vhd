00e358af1c5771936155d9739fcc0f1a6dd7e49d
MichaelBarrett219 <michaelbarrett000@gmail.com>
2018-12-01 20:24:43 -0500

    alu wrote up to mpyu

--- alu.vhd
+++ alu.vhd
@@ -573,9 +573,11 @@ architecture behavioral of alu is
 
       -- multiply unsigned: the 16 rightmost bits of each of the four 32-bit slots in registers rs1 are multiplied by the 16 rightmost bits of the corresponding 32-bit slots in register rs2, treating both operands as unsigned. The four 32-bit products are placed into the corresponding slots of register rd. (Comments: 4 separate 32-bit values in each 128-bit register)
       elsif instr_num = 19 then -- mpyu
+        res <= (unsigned(s1_lf4) * unsigned(s2_lf4)) & (unsigned(s1_lf3) * unsigned(s2_lf3)) & (unsigned(s1_lf2) * unsigned(s2_lf2)) & (unsigned(s1_lf1) * unsigned(s2_lf1));
 
       -- absolute difference of bytes: the contents of each of the 16 byte slots in register rs2 is subtracted from the contents of the corresponding byte slot in register rs1. The absolute value of each of the results is placed into the corresponding byte slot in register rd. (Comments: 16 separate 8-bit values in each 128-bit register)
       elsif instr_num = 20 then -- absdb
+        res <= (abs(signed(s1_hf4) - signed(s2_hf4)) & abs(signed(s1_lf4) - signed(s2_lf4)) & abs(signed(s1_hf3) - signed(s2_hf3)) & abs(signed(s1_lf3) - signed(s2_lf3)) & abs(signed(s1_hf2) - signed(s2_hf2)) & abs(signed(s1_lf2) - signed(s2_lf2)) & abs(signed(s1_lf1) - signed(s2_lf1)));
 
       end if;
     end process AluProc; 
