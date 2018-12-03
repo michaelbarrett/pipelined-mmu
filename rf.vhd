d5debe7d455c3be442c8ca8f9e332a8e9dfc1cff
michael barrett <michaelbarrett000@gmail.com>
2018-12-02 19:06:51 -0500

    Update rf.vhd

--- rf.vhd
+++ rf.vhd
@@ -24,7 +24,10 @@ architecture rtl of rf is
 
   -- array holds 32 128-bit registers -- 0 to 31 is (0 to (2**address'length)-1)
   type data_type is array (0 to 31) of std_logic_vector(127 downto 0);
-  signal data : data_type;
+  -- initialize to zero
+  signal data : data_type := (others=>(others=>'0'));
+  
+  -- other signals
   signal rd_temp : std_logic_vector(4 downto 0);
   signal read_address1 : std_logic_vector(4 downto 0);
   signal read_address2 : std_logic_vector(4 downto 0);
@@ -35,6 +38,12 @@ begin
   RfProc : process(r_addr1, r_addr2, r_addr3, w_addr1, w_data_in1) is
 
   begin
+	-- register write (no if statement bc always) -- use rd_temp so that it's available next cycle for wb
+    data(to_integer(unsigned(rd_temp))) <= w_data_in1;
+
+    rd_temp <= w_addr1;
+	
+	-- register read
     read_address1 <= r_addr1;
     read_address2 <= r_addr2;
     read_address3 <= r_addr3;
@@ -42,11 +51,6 @@ begin
     r_data_out1 <= data(to_integer(unsigned(read_address1)));
     r_data_out2 <= data(to_integer(unsigned(read_address2)));
     r_data_out3 <= data(to_integer(unsigned(read_address3)));  
-	
-	-- register write (no if statement bc always) -- use rd_temp so that it's available next cycle for wb
-    data(to_integer(unsigned(rd_temp))) <= w_data_in1;
-
-    rd_temp <= w_addr1;
 
   end process RfProc;
 
